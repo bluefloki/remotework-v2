a1ca15af42bfe94d7ff6578d6eb47175ae1d1eb2
meerankazmi <kazmimeeran@gmail.com>
2020-10-28 14:13:05 +0500

    Added build folder

---
+++ client/build/precache-manifest.7697155965e00e2ec26d8749d535079b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c83e5fc503fee4f2cdd99f5ad76bb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "231a8dc04008b7003eaf",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "7091548116b7acbfb90b",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "231a8dc04008b7003eaf",
+    "url": "/static/js/2.17909c3c.chunk.js"
+  },
+  {
+    "revision": "fd295cb0e0f176182e3e79d3bd774484",
+    "url": "/static/js/2.17909c3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7091548116b7acbfb90b",
+    "url": "/static/js/main.b11c2916.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "232975cb19a9bedd17e67e1f307a7a01",
+    "url": "/static/media/1.232975cb.txt"
+  },
+  {
+    "revision": "3c29098bf23e23d1806b21c53b4309e3",
+    "url": "/static/media/hero.3c29098b.jpg"
+  }
+]);
